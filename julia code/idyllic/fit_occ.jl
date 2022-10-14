aa7e0cd0b81b4c4226ffce0061a8997d93d9ba6c
Christian Stratton <3strattonc@gmail.com>
2022-10-14 13:18:07 -0600

    literally fixed everything lol

--- julia code/idyllic/fit_occ.jl
+++ julia code/idyllic/fit_occ.jl
@@ -47,6 +47,7 @@ function export_to_R(dict, filename)
   R"saveRDS(dict, file = filename)"
 end
 
+## model0
 @everywhere function fit_occ(seed, num_mcmc, thin, data_list; warmup = num_mcmc/2)
 
 	# error checking
@@ -56,37 +57,22 @@ end
 
 	# set seed
 	Random.seed!(seed)
-	τ_a, τ_b = (1, 1)
 
 	# housekeeping
 	X = data_list["X"]
 	y = data_list["y"]
 	J = data_list["J"]
-	C = data_list["C"]
- 	μ = data_list["expert_means"]
-	σ = data_list["expert_sds"]
-	Σ = Diagonal(σ)
-	Σinv = inv(Σ)
-	Σinvμ = Σinv * μ
 	n = size(X)[1]
 	p = size(X)[2]
-	K = size(μ, 1)
 
 	# storage
-	τ_mcmc = zeros(Int((num_mcmc - warmup)/thin), 1)
 	β_mcmc = zeros(Int((num_mcmc - warmup)/thin), p)
-	θ_mcmc = zeros(Int((num_mcmc - warmup)/thin), K)
-	η_mcmc = zeros(Int((num_mcmc - warmup)/thin), n)
-	z_mcmc = zeros(Int((num_mcmc - warmup)/thin), n)
+	ψ_mcmc = zeros(Int((num_mcmc - warmup)/thin), n)
 	p_mcmc = zeros(Int((num_mcmc - warmup)/thin), 1)
 
 	# intialize
-	τ = first(rand(InverseGamma(τ_a, τ_b)))
-	τ2 = dot(τ, τ)
 	β = rand(MvNormal(zeros(p), 2 .* I(p)))
-	θ = vec(rand(MvNormal(μ, Σ), 1))
-	η = vec(rand(MvNormal(vec(C*θ), Diagonal(τ2 .* ones(n))), 1))
-	linpred = X * β + η
+	linpred = X * β
 	ψ = exp.(linpred) ./ (1 .+ exp.(linpred))
 	z = fill(0, n)
 	for i in 1:n
@@ -97,9 +83,6 @@ end
 	# convenience
 	Xt = transpose(X)
 	XtX = transpose(X) * X
-	Ct = transpose(C)
-	CtC = transpose(C) * C
-
 
 	# sample
 	progress_bar = Progress(num_mcmc, dt = 1, desc = string("Progress: "))
@@ -117,33 +100,155 @@ end
 
 		# pg stuff
 		κ = z .- .5
-		ω = sample_pg(n, ones(n), X * β + η)
+		ω = sample_pg(n, ones(n), X * β)
 		Ω = Diagonal(ω)
 		z_ = κ ./ ω
 
 		# beta
 		V_β = inv(Xt * Ω * X + Diagonal(.5 .* ones(p))); V_β = Symmetric(V_β)
-		m_β = V_β * (Xt * Ω * (z_ - η))
+		m_β = vec(V_β * (Xt * κ))
+		β = rand(MvNormal(m_β, V_β), 1)
+		linpred = X * β
+		ψ = exp.(linpred) ./ (1 .+ exp.(linpred))
+
+		# p
+		a = 1 + dot(z, y)
+		b = 1 + dot(z, fill(J, n) - y)
+		detect_p = first(rand(Beta(a, b)))
+
+		# store, accounting for thinning
+		if (iter > warmup) & ((iter-warmup) % thin == 0)
+			β_mcmc[Int((iter-warmup)/thin),1:p] = β[1:p]
+			ψ_mcmc[Int((iter-warmup)/thin),1:n] = ψ
+			p_mcmc[Int((iter-warmup)/thin),1] = detect_p
+		end
+
+		# increment progress
+		next!(progress_bar)
+	end
+
+	# names
+	betanames = repeat(["f"], p)
+	for ndx in 1:p
+		betanames[ndx] = string("beta[", ndx, "]")
+	end
+
+	ψnames = repeat(["f"], n)
+	for ndx in 1:n
+		ψnames[ndx] = string("psi[", ndx, "]")
+	end
+
+	names = ["p"; betanames; ψnames]
+	out = Dict()
+	out["samples"] = hcat(p_mcmc, β_mcmc, ψ_mcmc)
+	out["names"] = names
+
+	return(out)
+end
+
+# fit model
+@everywhere curry(f, xs...) = y -> f(y, xs...)
+fit_parallel = pmap(curry(fit_occ, 10000, 2, data_list), [1 2 3])
+mcmctoR(fit_parallel, "model0.rds")
+
+
+## model1
+@everywhere function fit_occ_hier(seed, num_mcmc, thin, data_list; warmup = num_mcmc/2)
+
+	# error checking
+	if (num_mcmc % thin) != 0
+		error("Error: Number of iterations not divisible by thinning interval.")
+	end
+
+	# set seed
+	Random.seed!(seed)
+	τ_a, τ_b = (1, 1)
+
+	# housekeeping
+	X = data_list["X"]
+	y = data_list["y"]
+	J = data_list["J"]
+	C = data_list["C"]
+ 	m = data_list["expert_means"]
+	s = data_list["expert_sds"]
+	Σ = Diagonal(s .* s)
+	Σinv = inv(Σ)
+	Σinvm = Σinv * m
+	n = size(X)[1]
+	p = size(X)[2]
+	K = size(m, 1)
+
+	# storage
+	τ_mcmc = zeros(Int((num_mcmc - warmup)/thin), 1)
+	β_mcmc = zeros(Int((num_mcmc - warmup)/thin), p)
+	θ_mcmc = zeros(Int((num_mcmc - warmup)/thin), K)
+	μ_mcmc = zeros(Int((num_mcmc - warmup)/thin), K)
+	ψ_mcmc = zeros(Int((num_mcmc - warmup)/thin), n)
+	p_mcmc = zeros(Int((num_mcmc - warmup)/thin), 1)
+
+	# intialize
+	τ = first(rand(InverseGamma(τ_a, τ_b)))
+	τ2 = dot(τ, τ)
+	μ = vec(rand(MvNormal(m, Σ)))
+	β = rand(MvNormal(zeros(p), 2 .* I(p)))
+	θ = vec(rand(MvNormal(μ, τ2 .* I(K)), 1))
+	linpred = X * β + C * θ
+	ψ = exp.(linpred) ./ (1 .+ exp.(linpred))
+	z = fill(0, n)
+	for i in 1:n
+		z[i] = first(rand(Binomial(1, ψ[i]), 1))
+	end
+	detect_p = .5
+
+	# convenience
+	Xt = transpose(X)
+	XtX = transpose(X) * X
+	Ct = transpose(C)
+	CtC = transpose(C) * C
+
+	# sample
+	progress_bar = Progress(num_mcmc, dt = 1, desc = string("Progress: "))
+	for iter in 1:num_mcmc
+
+		# sample z
+		for i in 1:n
+			if y[i] != 0
+				z[i] = 1
+			else
+				tmp = ψ[i] * detect_p^J
+				z[i] = first(rand(Binomial(1, tmp / (1 - ψ[i] + tmp))))
+			end
+		end
+
+		# pg stuff
+		κ = z .- .5
+		ω = sample_pg(n, ones(n), X * β + C * θ)
+		Ω = Diagonal(ω)
+		z_ = κ ./ ω
+
+		# beta
+		V_β = inv(Xt * Ω * X + Diagonal(.5 .* ones(p))); V_β = Symmetric(V_β)
+		m_β = vec(V_β * (Xt * Ω * (z_ - C * θ)))
 		β = rand(MvNormal(m_β, V_β), 1)
 
-		# eta
-		V_η = inv(Ω + Diagonal(1/τ2 .* ones(n))); V_η = Symmetric(V_η)
-		m_η = vec(V_η * (Ω * (z_ - X*β) + 1/τ2 .* C * θ))
-		η = rand(MvNormal(m_η, V_η))
-
 		# theta
-		V_θ = inv(1/τ2 .* CtC + Σinv); V_θ = Symmetric(V_θ)
-		m_θ = vec(V_θ * (1/τ2 .* Ct * η + Σinvμ))
+		V_θ = inv(Ct * Ω * C + (1/τ2) .* I(K)); V_θ = Symmetric(V_θ)
+		m_θ = vec(V_θ * (Ct * Ω * (z_ - X * β) + (1/τ2) .* μ))
 		θ = rand(MvNormal(m_θ, V_θ), 1)
+		linpred = X * β + C * θ
+		ψ = exp.(linpred) ./ (1 .+ exp.(linpred))
+
+		# mu
+		V_μ = inv(1/τ2 .* I(K) + Σinv); V_μ = Symmetric(V_μ)
+		m_μ = vec(V_μ * (1/τ2 .* θ + Σinvm))
+		μ = rand(MvNormal(m_μ, V_μ))
 
 		# tau
-		# SSE = first(transpose(η - C * θ) * (η - C * θ))
-		# a = τ_a + n/2
-		# b = τ_b + .5 * SSE
-		# τ2 = rand(InverseGamma(a, b))
-		# τ = sqrt(τ2)
-		τ2 = 1
-		τ = 1
+		SSE = first(transpose(θ - μ) * (θ - μ))
+		a = τ_a + K/2
+		b = τ_b + .5 * SSE
+		τ2 = rand(InverseGamma(a, b))
+		τ = sqrt(τ2)
 
 		# p
 		a = 1 + dot(z, y)
@@ -155,8 +260,8 @@ end
 			τ_mcmc[Int((iter-warmup)/thin),1] = τ
 			β_mcmc[Int((iter-warmup)/thin),1:p] = β[1:p]
 			θ_mcmc[Int((iter-warmup)/thin),1:K] = θ[1:K]
-			η_mcmc[Int((iter-warmup)/thin),1:n] = η
-			z_mcmc[Int((iter-warmup)/thin),1:n] = z
+			μ_mcmc[Int((iter-warmup)/thin),1:K] = μ[1:K]
+			ψ_mcmc[Int((iter-warmup)/thin),1:n] = ψ
 			p_mcmc[Int((iter-warmup)/thin),1] = detect_p
 		end
 
@@ -180,14 +285,14 @@ end
 		thetanames[ndx] = string("theta[", ndx, "]")
 	end
 
-	znames = repeat(["f"], n)
+	ψnames = repeat(["f"], n)
 	for ndx in 1:n
-		znames[ndx] = string("z[", ndx, "]")
+		ψnames[ndx] = string("psi[", ndx, "]")
 	end
 
-	names = ["tau"; betanames; thetanames; etanames; znames; "p"]
+	names = ["p";"tau"; betanames; thetanames; ψnames]
 	out = Dict()
-	out["samples"] = hcat(τ_mcmc, β_mcmc, θ_mcmc, η_mcmc, z_mcmc, p_mcmc)
+	out["samples"] = hcat(p_mcmc, τ_mcmc, β_mcmc, θ_mcmc, ψ_mcmc)
 	out["names"] = names
 
 	return(out)
@@ -195,5 +300,5 @@ end
 
 # fit model
 @everywhere curry(f, xs...) = y -> f(y, xs...)
-fit_parallel = pmap(curry(fit_occ, 50000, 5, data_list), [1 2 3])
+fit_parallel = pmap(curry(fit_occ_hier, 10000, 2, data_list), [1 2 3])
 mcmctoR(fit_parallel, "expert_hier_idyllic.rds")
--- julia code/idyllic/stan_occ.stan
+++ julia code/idyllic/stan_occ.stan
@@ -1,44 +1,61 @@
+// consider https://mc-stan.org/docs/stan-users-guide/multivariate-hierarchical-priors.html
+// for more complexity and dependence among theta
 data {
   int<lower=1> N; // Number of sites
-  int<lower=1> J; // Number of temporal replications
-  int<lower=0> sumy[N]; // Observation
-  real x[N]; 
+  int<lower=1> J; // Number of visits (fixed for now, doesnt have to be)
   int<lower = 1> K; // Number of categories
-  int<lower=0, upper=K> group[N]; 
-  real mu[K];
-  real sigma[K];
+  int<lower=0, upper=K> group[N];  // Indicator vector
+  int<lower=0> sumy[N]; // Sum of obs for all visits
+  real x[N]; 
+  real m[K]; // prior means from mapping the experts
+  real<lower = 0> s[K]; // prior sds from mapping the experts
 }
 parameters {
   real beta0;
   real beta1;
-  real eta[N];
   real theta[K];
-  
+  real mu[K];
   real<lower=0, upper=1> p; // Detection probability
-  //real<lower=0> tau;
+  real<lower=0> tau;
 }
 model {
-  // Priors
-  for (k in 1:K){
-    theta[k] ~ normal(mu[k], sigma[k]);
+  // hyper priors
+  for(k in 1:K){
+    mu[k] ~ normal(m[k], s[k]);
   }
+
+  // hierarchical prior
+  for (k in 1:K){
+    theta[k] ~ normal(mu[k], tau);
+  }
+  
+  // other stuff
   beta0 ~ normal(0, 2);
   beta1 ~ normal(0, 2);
- // tau ~ normal(0, 1);
-  
+    
   // Likelihood
   for (i in 1:N) {
-    eta[i] ~ normal(theta[group[i]], 1);
     if(sumy[i] > 0) {
       // Occurred and observed
-      1 ~ bernoulli(inv_logit(beta0 + beta1 * x[i] + eta[i]));
+      1 ~ bernoulli(inv_logit(beta0 + beta1 * x[i] + theta[group[i]]));
       sumy[i] ~ binomial(J, p);
     } else {
-      // Occurred and not observed
-      target += log_sum_exp(bernoulli_lpmf(1 | inv_logit(beta0 + beta1 * x[i] + eta[i]))
-                            + bernoulli_lpmf(0 | p) * J,
-                            // Not occurred
-                            bernoulli_lpmf(0 | inv_logit(beta0 + beta1 * x[i] + eta[i])));
+      target += log_sum_exp(
+        // Occurred and not observed
+        bernoulli_lpmf(1 | inv_logit(beta0 + beta1 * x[i] + theta[group[i]])) + bernoulli_lpmf(0 | p) * J,
+        // Not occurred
+        bernoulli_lpmf(0 | inv_logit(beta0 + beta1 * x[i] + theta[group[i]]))
+      );
     }
   }
 }
+generated quantities{
+  real<lower = 0, upper = 1> psi[N];
+  for(i in 1:N){
+    psi[i] = inv_logit(beta0 + beta1 * x[i] + theta[group[i]]);
+  }
+}
+
+
+
+
